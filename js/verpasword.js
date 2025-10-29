a511f4efbc84685cee47ca8b5fa5c75be120aab1
YEISONGS <yeison2012199413@gmail.com>
2025-10-28 20:37:25 -0500

    funcionaliodad jsd

--- js/verpasword.js
+++ js/verpasword.js
@@ -1,3 +1,8 @@
-function validar(){
-    Swal.fire("SweetAlert2 is working!");
-}
+const password = docunment.getElementById("password");
+
+toggle_password.addEventListener("click",()=>{
+    const type = password.type === "password"  ? "text" : "password";
+    password.type = tipo_elemento;
+    toggle_password.classList.toggle("fa-eye");
+    toggle_password.classList.toggle("fa-eye-slash");
+})
