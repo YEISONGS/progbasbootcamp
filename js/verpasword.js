3ecb341161a1789b9bf1148cecf7dfc9de357ff5
YEISONGS <yeison2012199413@gmail.com>
2025-10-29 20:47:57 -0500

    funcacionalidad js

--- js/verpasword.js
+++ js/verpasword.js
@@ -1,8 +1,32 @@
-const password = docunment.getElementById("password");
+const password = document.getElementById("password");
 
-toggle_password.addEventListener("click",()=>{
-    const type = password.type === "password"  ? "text" : "password";
+toggle_password.addEventListener("click", () => {
+    const tipo_elemento = password.type === "password" ? "text" : "password";
     password.type = tipo_elemento;
     toggle_password.classList.toggle("fa-eye");
     toggle_password.classList.toggle("fa-eye-slash");
 })
+
+function validar(){
+    let password = document.getElementById("password").value;
+    let lowercase = /[a-z]/.test(password);
+    let uppercase = /[A-Z]/.test(password);
+    let number = /\d/.test(password);
+    let specialchar = /[\W_]/.test(password);
+    // alert(password.length);
+    if(password.length < 8){
+        Swal.fire(
+            "Error",
+            "La contraseña debe tener al menos 8 caracteres",
+            "error"
+        );
+        return
+    }else{
+        Swal.fire(
+            "Felicidades",
+            Su contraseña es correcta: ${password},
+            "success"
+        );
+        return
+    }
+}
